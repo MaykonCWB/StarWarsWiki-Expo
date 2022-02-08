f56eeaf3721bf959ceaa7f28b5bc2aa2a3859825
Maykon Melo <52245727+Maykonctba@users.noreply.github.com>
2022-02-08 15:00:13 -0300

    (Custom Fonts, Themes, Fonts, Responsive Screens and Navigation)

--- src/components/molecules/Card/styles.js
+++ src/components/molecules/Card/styles.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components/native'
 
-export const CardContainer = styled.view`
+export const CardContainer = styled.TouchableOpacity`
   width: ${({ theme }) => theme.metrics.px(88)}px;
-  width: ${({ theme }) => theme.metrics.px(124)}px;
+  height: ${({ theme }) => theme.metrics.px(124)}px;
   border-radius: ${({ theme }) => theme.metrics.px(8)}px;
+  overflow: hidden;
 `
 
-export const CardImage = styled.ImageBackground``
+export const CardImage = styled.ImageBackground`
+  width: 100%;
+  height: 100%;
+`
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
-import { Container } from '../..components'
+import { Container, Card } from '../../components'
 
 export const Home = () => {
-  return <Container />
+  return (
+    <Container align="center" justify="center">
+      <Card
+        item={{
+          image_url:
+            'https://23670.cdn.simplo7.net/static/23670/sku/quadros-quadro-decorativo-velcro-darth-vader-star-wars--p-1582384116874.jpg',
+        }}
+      />
+    </Container>
+  )
 }
